2d36160f75bffae861d076eb8368598570a8d5e8
MAYM7 <mmkoko8@gmail.com>
2023-08-30 20:00:44 +0200

    "nice"

--- git auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                                                                                                           
-Token=$(echo "[Your Token]")
-UserName=$(echo "[Your UserName]")
-RepoName=$(echo "[Your Repository Name]")
-git add $1
-git commit -m \"$2\"
-git push https://$Token@github.com/$UserName/$RepoName.git
